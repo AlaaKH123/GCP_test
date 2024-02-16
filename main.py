8df6a5c3e1269f12f7b05f79048a7ed02da96351
Administrator <admin@example.com>
2024-02-16 14:48:07 +0000

    Salut

--- main.py
+++ main.py
@@ -19,30 +19,28 @@ def testpython(event, context):
          context (google.cloud.functions.Context): Metadata for the event.
     """
 
-    lst = []
     file_name = event['name']
     table_name = file_name.split('.')[0]
+    file_path = 'gs://' + event['bucket'] + '/streaming/' + event['name']
 
-    # Event,File metadata details writing into Big Query
-    dct={
-         'Event_ID':context.event_id,
-         'Event_type':context.event_type,
-         'Bucket_name':event['bucket'],
-         'File_name':event['name'],
-         'Created':event['timeCreated'],
-         'Updated':event['updated']
-        }
-    lst.append(dct)
-    df_metadata = pd.DataFrame.from_records(lst)
-    df_metadata.to_gbq('test_data_platforme.data_loading_metadata', 
-                        project_id='fivetran-408613', 
-                        if_exists='append',
-                        location='europe-west1')
-    
+    # Event,File metadata details writing into BigQuery
+    dct = {
+        'Event_ID': context.event_id,
+        'Event_type': context.event_type,
+        'Bucket_name': event['bucket'],
+        'File_name': event['name'],
+        'Created': event['timeCreated'],
+        'Updated': event['updated']
+    }
+    df_metadata = pd.DataFrame.from_records([dct])
+    df_metadata.to_gbq('test_data_platforme.data_loading_metadata',
+                       project_id='fivetran-408613',
+                       if_exists='append',
+                       location='europe-west1')
+
     # Actual file data , writing to Big Query
-    df_data = pd.read_csv('gs://' + event['bucket'] + '/' + file_name)
-
-    df_data.to_gbq('test_data_platforme.' + table_name, 
-                        project_id='fivetran-408613', 
-                        if_exists='append',
-                        location='europe-west1')
+    df_data = pd.read_csv(file_path)
+    df_data.to_gbq('test_data_platforme.' + table_name,
+                   project_id='fivetran-408613',
+                   if_exists='append',
+                   location='europe-west1')
